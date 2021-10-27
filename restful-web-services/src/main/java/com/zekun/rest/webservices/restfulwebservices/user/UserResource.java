ce46e4975f02c296b3fc9db790f70077d3928d20
guozekun <woxinguangmingguo@gmail.com>
2021-10-28 00:57:30 +0800

    update createUser api

--- restful-web-services/src/main/java/com/zekun/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/zekun/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,8 +1,11 @@
 package com.zekun.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,8 +31,18 @@ public class UserResource {
 
     //POST /users
     //void createUser(User user)
+    //输入 用户的细节
+    //输出 已创建 & 返回创建的URI
     @PostMapping("/users")
-    public void createUser(@RequestBody  User user) {
+    public ResponseEntity<Object> createUser(@RequestBody  User user) {
         User savedUser = useDaoService.save(user);
+        //Created
+        // /users/{id}  -> savedUsers.getId()
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
